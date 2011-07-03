a25da3879de35a7c58c4be7efe26fcd42886d990
beepscore <support@beepscore.com>
2011-07-02 20:01:09 -0700

    Add MVC Model class CalculatorBrain.

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,16 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Steve Baker on 7/2/11.
+//  Copyright 2011 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CalculatorBrain : NSObject {
+    
+}
+
+@end
---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,14 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by Steve Baker on 7/2/11.
+//  Copyright 2011 Beepscore LLC. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+
+@implementation CalculatorBrain
+
+@end
